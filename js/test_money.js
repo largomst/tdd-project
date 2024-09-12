718c4b268f99a9296926e0d5a2da82301031237b
largomst <dingrui.zhen@gmail.com>
2024-09-12 10:26:10 +0800

    feat: pass first js test

--- js/test_money.js
+++ js/test_money.js
@@ -1,5 +1,15 @@
 const assert = require('assert');
 
-let fiver = new Dollor(5);
-let tener = new fiver.timer(10);
+class Dollar {
+    constructor(amount) {
+        this.amount = amount;
+    }
+    times(multiplier) {
+        return new Dollar(10);
+    }
+}
+
+let fiver = new Dollar(5);
+let tener = fiver.times(10);
 assert.strictEqual(tener.amount, 10);
+
