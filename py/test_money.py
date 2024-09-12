58cfe92b1fe62b8561e8a9b58ed25a3e9df9a28b
largomst <dingrui.zhen@gmail.com>
2024-09-12 15:04:38 +0800

    refactor: moved Money and Portfolio class their own Python files

--- py/test_money.py
+++ py/test_money.py
@@ -1,34 +1,7 @@
-import functools
-import operator
 import unittest
 
-
-class Money:
-    def __init__(self, amount, currency):
-        self.amount = amount
-        self.currency = currency
-
-    def times(self, multiplier):
-        return Money(self.amount * multiplier, self.currency)
-
-    def divide(self, divisor):
-        return Money(self.amount / divisor, self.currency)
-
-    def __eq__(self, other):
-        return self.amount == other.amount and self.currency == other.currency
-
-
-class Portfolio:
-    def __init__(self):
-        self.moneys = []
-
-    def add(self, *moneys):
-        self.moneys += moneys
-
-    def evaluate(self, currency):
-        total = functools.reduce(operator.add, map(lambda m: m.amount, self.moneys), 0)
-        return Money(total, currency)
-
+from money import Money
+from portfolio import Portfolio
 
 class TestMoney(unittest.TestCase):
     def testMultiplicationInDollar(self):
