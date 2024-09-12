2158a8a6ab87387ef6c67a321dcdcb9e7f4f5d48
largomst <dingrui.zhen@gmail.com>
2024-09-12 11:05:55 +0800

    feat: implement Monoey

--- go/money_test.go
+++ go/money_test.go
@@ -13,6 +13,17 @@ func TestMultiplication(t *testing.T) {
 		t.Errorf("Expected 10, got: [%d]", tenner.amount)
 	}
 }
+func TestMultiplicationInEuros(t *testing.T) {
+	tenEuros := Money{amount: 10, currency: "EUR"}
+	twentyEuros := tenEuros.Times(2)
+	if twentyEuros.amount != 20 {
+		t.Errorf("Expected 20, got: [%d]", twentyEuros.amount)
+	}
+	if twentyEuros.currency != "EUR" {
+		t.Errorf("Expected EUR, got: [%s]", twentyEuros.currency)
+	}
+
+}
 
 type Dollar struct {
 	amount int
@@ -20,4 +31,13 @@ type Dollar struct {
 
 func (d Dollar) Times(multiplier int) Dollar {
 	return Dollar{d.amount * multiplier}
+}
+
+type Money struct {
+	amount   int
+	currency string
+}
+
+func (m Money) Times(multiplier int) Money {
+	return Money{m.amount * multiplier, m.currency}
 }
--- js/test_money.js
+++ js/test_money.js
@@ -9,7 +9,22 @@ class Dollar {
     }
 }
 
+class Money { 
+    constructor(amount, currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+    times(multiplier) {
+        return new Money(this.amount * multiplier, this.currency);
+    }
+}
+
 let fiver = new Dollar(5);
 let tener = fiver.times(2);
 assert.strictEqual(tener.amount, 10);
+
+let tenEuros = new Money(10, "EUR");
+let twentyEuros = tenEuros.times(2);
+assert.strictEqual(twentyEuros.amount, 20);
+assert.strictEqual(twentyEuros.currency, "EUR");
 
